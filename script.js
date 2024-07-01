caa1a27cb0312b890b87d505b870448c0dab2789
maheshbasnet089 <basnetmanish089@gmail.com>
2024-07-01 15:06:08 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// var bottle = "paani"
+// let bottle = 'paani'
+// const bottle  = 'paani'
+
+const friend1 = 'Pranjal'
+const friend2 = 'Bob'
+const friend3 = 'Anup'
+
+
+const friends = ['pranjal','bob','anup',12,true]
+
+const name = "Manish Basnet"
+const age = 22 
+const address = 'Itahari'
+
+const me = {
+    name : 'Manish Basnet',
+    age : 22, 
+    address : 'Itahari'
+}
+
+
+
